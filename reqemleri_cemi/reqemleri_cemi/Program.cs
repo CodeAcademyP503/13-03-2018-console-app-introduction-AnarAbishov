5e7738b04830027491f232f5792da9a2e18d74a6
AnarAbishov <mr.avinar@mail.ru>
2018-03-17 04:19:11 +0400

    Add files via upload

---
+++ reqemleri_cemi/reqemleri_cemi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reqemleri_cemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void hesablama()
+            {
+                Console.WriteLine("Reqem daxil edin");
+
+                Console.WriteLine("---------------------");
+
+                var n = Convert.ToInt32(Console.ReadLine());
+
+                if (n>0)
+                {
+                    var total = 0;
+                    for (var i = 0; n >= 1; i++)
+                    {
+                        total = total + n % 10;
+                        n = (n / 10);
+                    }
+                    Console.WriteLine("---------------------");
+
+                    Console.WriteLine("Yazilan ededin reqemlerinin cemi : " + total);
+                    Console.WriteLine("---------------------");
+                    hesablama();
+                }
+                
+                else{
+                    Console.WriteLine("---------------------");
+                    Console.WriteLine("Duzgun reqem yazin!!!");
+                    Console.WriteLine("---------------------");
+                    hesablama();
+                }
+
+            }
+            hesablama();
+            
+            
+        }
+    }
+}
